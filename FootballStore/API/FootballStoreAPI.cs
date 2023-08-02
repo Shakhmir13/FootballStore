297031fcb0c54cfc75b3687a1e9a048b3053a58f
Shakhmir <paltushev97@mail.ru>
2023-08-02 22:38:16 +0600

    API

--- FootballStore/API/FootballStoreAPI.cs
+++ FootballStore/API/FootballStoreAPI.cs
@@ -1,4 +1,5 @@
-﻿using FootballStore.DataAccess.Repository.Interfaces;
+﻿using AspNetCore;
+using FootballStore.DataAccess.Repository.Interfaces;
 using FootballStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using NuGet.Packaging.Signing;
@@ -7,12 +8,12 @@ using NuGet.Packaging.Signing;
 
 namespace FootballStore.API
 {
-    [Route("api/products")]
     [ApiController]
-    public class FootballStoreAPI : ControllerBase
+    [Route("api/products")]
+    public class ProductController : ControllerBase
     {
         private IActionUnit _action;
-        public FootballStoreAPI(IActionUnit action)
+        public ProductController(IActionUnit action)
         {
             _action = action;
         }
@@ -54,7 +55,6 @@ namespace FootballStore.API
             return Ok(product);
         }
 
-        // DELETE api/<API>/5
         [HttpDelete("{id}")]
         public IActionResult DeleteProductById(int id)
         {
@@ -63,6 +63,124 @@ namespace FootballStore.API
             _action.Product.Delete(product);
             _action.Save();
             return Ok("Продукт успешно удален.");
+        }
+    }
+
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoryController : ControllerBase
+    {
+        private IActionUnit _action;
+        public CategoryController(IActionUnit action)
+        {
+            _action = action;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            var categories = _action.Category.GetAll();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _action.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewCategory([FromBody] Category category)
+        {
+            if (category == null) { return BadRequest("Некорректные данные"); }
+            _action.Category.Add(category);
+            _action.Save();
+            return CreatedAtAction(nameof(GetCategoryById), new { id = category.Id }, category);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateCategoryById(int id, [FromBody] Category updatedcategory)
+        {
+            var category = _action.Category.GetT(x => x.Id == id);
+            if (category == null) { return NotFound("Продукт не найден."); }
+            _action.Category.Update(updatedcategory);
+            _action.Save();
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCategoryById(int id)
+        {
+            var category = _action.Category.GetT(x => x.Id == id);
+            if (category == null) { return NotFound("Продукт не найден."); }
+            _action.Category.Delete(category);
+            _action.Save();
+            return Ok("Продукт успешно удален.");
+        }
+    }
+
+    [ApiController]
+    [Route("api/orders")]
+    public class OrderController : ControllerBase
+    {
+        private IActionUnit _action;
+        public OrderController(IActionUnit action)
+        {
+            _action = action;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOrders()
+        {
+            var orders = _action.Order.GetAll();
+            return Ok(orders);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOrderById(int id)
+        {
+            var order = _action.Order.GetT(x => x.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewOrder([FromBody] Order order)
+        {
+            if (order == null) { return BadRequest("Некорректные данные"); }
+            _action.Order.Add(order);
+            _action.Save();
+            return CreatedAtAction(nameof(GetOrderById), new { id = order.Id }, order);
+        }
+
+        [ApiController]
+        [Route("api/orderdetails")]
+        public class OrderDetailsController : ControllerBase
+        {
+            private IActionUnit _action;
+            public OrderDetailsController(IActionUnit action)
+            {
+                _action = action;
+            }
+
+            [HttpGet("{id}")]
+            public IActionResult GetOrderDetailsByOrderId(int id)
+            {
+                var orderdetails = _action.OrderDetail.GetT(x => x.OrderId == id);
+                if (orderdetails == null)
+                {
+                    return NotFound();
+                }
+                return Ok(orderdetails);
+            }
         }
     }
 }
